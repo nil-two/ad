ea81bbc8d106e7be27cddf123ae01a4f24d789e8
nil2 <nil2@nil2.org>
2021-12-05 00:58:25 +0900

    Unify embed script

--- script/embed_appscript.bash
+++ script/embed_appscript.bash
@@ -3,12 +3,13 @@ set -eu
 
 cd -- "$(dirname -- "${BASH_SOURCE[0]}")/.."
 
+target_file=$(basename "$PWD")
 embed_marker_start='" appscript:start'
 embed_marker_end='" appscript:end'
-embed_file='src/app.vim'
+embed_file="src/$(basename "$PWD").vim"
 sed -i \
   -e '/^'"$embed_marker_start"'$/r '"$embed_file"'' \
   -e '/^'"$embed_marker_start"'$/i'"$embed_marker_start"'' \
   -e '/^'"$embed_marker_end"'$/i'"$embed_marker_end"'' \
   -e '/^'"$embed_marker_start"'$/,/^'"$embed_marker_end"'$/d' \
-  ad
+  "$target_file"
